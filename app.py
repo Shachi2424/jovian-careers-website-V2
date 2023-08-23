b54ff32377ad496f3a7bad8f26138dc1a64ad10a
Shachi2424 <141862320+Shachi2424@users.noreply.github.com>
2023-08-23 14:47:55 +0530

    commit
    commit to main

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template
 app = Flask(__name__)
 JOBS =[{'id':1,
         'title': 'Data Analyst',
@@ -9,14 +9,13 @@ JOBS =[{'id':1,
          'location': 'Delhi',
          'salary': 'Rs. 11,00,000'
          }]
-@app.route("/api/jobs")  
-def list_jobs():
-    return jsonify(JOBS)
 
 def hello_world():
     return render_template('home.html',jobs=JOBS)
 
 @app.route("/Jobs")
+def display_jobs():  
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0',debug=True)
